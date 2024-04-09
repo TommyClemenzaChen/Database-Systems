39e657485a7cf527c681da36a2d37710803798f4
Tommy Chen <tchen175@ucsc.edu>
2024-04-09 13:45:01 -0700

    codebase

---
+++ Paged-File-Manager/Codebase/rbf/pfm.cc
@@ -0,0 +1,88 @@
+#include "pfm.h"
+
+PagedFileManager* PagedFileManager::_pf_manager = 0;
+
+PagedFileManager* PagedFileManager::instance()
+{
+    if(!_pf_manager)
+        _pf_manager = new PagedFileManager();
+
+    return _pf_manager;
+}
+
+
+PagedFileManager::PagedFileManager()
+{
+}
+
+
+PagedFileManager::~PagedFileManager()
+{
+}
+
+
+RC PagedFileManager::createFile(const string &fileName)
+{
+    return -1;
+}
+
+
+RC PagedFileManager::destroyFile(const string &fileName)
+{
+    return -1;
+}
+
+
+RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle)
+{
+    return -1;
+}
+
+
+RC PagedFileManager::closeFile(FileHandle &fileHandle)
+{
+    return -1;
+}
+
+
+FileHandle::FileHandle()
+{
+	readPageCounter = 0;
+	writePageCounter = 0;
+	appendPageCounter = 0;
+}
+
+
+FileHandle::~FileHandle()
+{
+}
+
+
+RC FileHandle::readPage(PageNum pageNum, void *data)
+{
+    return -1;
+}
+
+
+RC FileHandle::writePage(PageNum pageNum, const void *data)
+{
+    return -1;
+}
+
+
+RC FileHandle::appendPage(const void *data)
+{
+    return -1;
+}
+
+
+unsigned FileHandle::getNumberOfPages()
+{
+    return -1;
+}
+
+
+RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount)
+{
+	return -1;
+}
---
+++ Paged-File-Manager/Codebase/rbf/rbfm.cc
@@ -0,0 +1,47 @@
+#include "rbfm.h"
+
+RecordBasedFileManager* RecordBasedFileManager::_rbf_manager = 0;
+
+RecordBasedFileManager* RecordBasedFileManager::instance()
+{
+    if(!_rbf_manager)
+        _rbf_manager = new RecordBasedFileManager();
+
+    return _rbf_manager;
+}
+
+RecordBasedFileManager::RecordBasedFileManager()
+{
+}
+
+RecordBasedFileManager::~RecordBasedFileManager()
+{
+}
+
+RC RecordBasedFileManager::createFile(const string &fileName) {
+    return -1;
+}
+
+RC RecordBasedFileManager::destroyFile(const string &fileName) {
+    return -1;
+}
+
+RC RecordBasedFileManager::openFile(const string &fileName, FileHandle &fileHandle) {
+    return -1;
+}
+
+RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
+    return -1;
+}
+
+RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
+    return -1;
+}
+
+RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid, void *data) {
+    return -1;
+}
+
+RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor, const void *data) {
+    return -1;
+}
