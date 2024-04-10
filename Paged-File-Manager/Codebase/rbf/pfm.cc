4f34a37f85a56d1c23eed8111e7b794aa3cc8a16
Tommy Chen <tchen175@ucsc.edu>
2024-04-09 22:32:00 -0700

    commit

--- Paged-File-Manager/Codebase/rbf/pfm.cc
+++ Paged-File-Manager/Codebase/rbf/pfm.cc
@@ -1,4 +1,7 @@
 #include "pfm.h"
+#include <stdio.h>
+#include <sys/stat.h>
+
 
 PagedFileManager* PagedFileManager::_pf_manager = 0;
 
@@ -23,18 +26,31 @@ PagedFileManager::~PagedFileManager()
 
 RC PagedFileManager::createFile(const string &fileName)
 {
-    return -1;
+    struct stat buffer;
+    if(stat(fileName.c_str(), &buffer) == 0){
+        return -1;
+    }
+    FILE * f = fopen(fileName.c_str(), "w");
+    fclose(f);
+
+    return 0;
 }
 
 
 RC PagedFileManager::destroyFile(const string &fileName)
 {
-    return -1;
+    struct stat buffer;
+    if(stat(fileName.c_str(), &buffer) == 0){
+        return -1;
+    }
+    remove(fileName.c_str());
+    return 0;
 }
 
 
 RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle)
 {
+    
     return -1;
 }
 
@@ -44,7 +60,7 @@ RC PagedFileManager::closeFile(FileHandle &fileHandle)
     return -1;
 }
 
-
+// only increment for a success file
 FileHandle::FileHandle()
 {
 	readPageCounter = 0;
@@ -63,7 +79,8 @@ RC FileHandle::readPage(PageNum pageNum, void *data)
     return -1;
 }
 
-
+// Figure out free space, do the insertion. Then find the next page with a free space
+// Create a new page if there is no space
 RC FileHandle::writePage(PageNum pageNum, const void *data)
 {
     return -1;
