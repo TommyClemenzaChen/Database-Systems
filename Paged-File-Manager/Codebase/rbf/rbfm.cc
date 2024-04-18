a836b2b60a49dc3d579148a2e683443a5a4ca5a5
Lola Quiroga <lolaquiroga25@gmail.com>
2024-04-18 15:04:09 -0700

    updated Record class but not working as is (comments in code for explanation)

--- Paged-File-Manager/Codebase/rbf/rbfm.cc
+++ Paged-File-Manager/Codebase/rbf/rbfm.cc
@@ -58,22 +58,78 @@ class Record {
         list<void *> recordList;
         int num_records;
         unsigned null_indicators;
+        vector<Attribute> &rd; 
+        const void *data;
+        RID &rid;
     };
     // initialize Record
 
     uint32_t calculateRecordSize(rec r) {
+        uint32_t size = 0;
+        for (int i = 0; i < r.num_records; i++) {
+            // Calculate size of each record and add it to the total size
+        }
+        return size;
+    }
+    
+    void* readNBytes(int charFlag; const void* recData, int n) {
+        // charFlag 0 = int/real (4 bytes)
+        // charFlag 1 = varchar (4 bytes + strlen)
         
+        if (charFlag == 0) {
+            void* buffer = new char[n]; // Allocate memory to store the read bytes
+
+            // Copy n bytes from recData into the buffer
+            memcpy(buffer, recData, n);
+
+            return buffer;
+        }
+        else {
+            const char* charData = static_cast<const char*>(recData);
+            char* buffer = new char[n]; // Allocate memory to store the read bytes
+
+            // Read n bytes
+            for (int i = 0; i < n; i++) {
+                buffer[i] = charData[i]; // Store the byte in the buffer
+            }
+
+            return buffer; // Return the buffer containing the read bytes
+        }
     }
 
-    protected:
-        Record() {
-            num_records = 0;
-            null_indicators = 0;
-            list.push(num_records);
-            list.push(null_indicators);
-        };                                   				// Constructor
-        ~Record();
+    // this will not work as is because:
+        // 1) data* has n byte null indicators for y fields that im not accounting for 
+        // 2) readNBytes reads from the beginning of the stream each time so it's not 
+            // getting the proper data every time its called
+    // solution? combine readNBytes with createRecord. splitting them up actually puts 
+        // us at a disadvantage. We need access to where data* left off.
+    void createRecord(void) {
+        // loop through vector to grab all the data
+            for (auto i = 0; i < rd.size(); i++) {
+                if (rd.at(i).type != TypeVarChar) {
+                    // read 4 bytes from data* and then append it to list.
+                    list.push(readNBytes(0, data, 4));
+                } else {
+                    // read 4+strlen bytes from data if the type is char.
+                    list.push(readNBytes(1, data, 4+rd.at(i).length))
+                }
+            }
+    }
 
+    
+
+    protected:
+        // Constructor
+        Record(int num_records, unsigned null_indicators, vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
+            num_records = num_records;
+            null_indicators = null_indicators;
+            vector<Attribute> &rd; 
+            const void *data = data;
+            RID &rid = rid;
+            recordList = {num_records, null_indicators}
+            
+        };                                   				
+        ~Record();
 }
 
 
